57ebcf7b59644d8949ffe3c87021d8484f12ca60
Makssell <nimbusbamboo@gmail.com>
2023-04-18 23:01:32 +0200

    pages

---
+++ sites/pages/Login.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { useNavigate, Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, storage, db } from "../../Projects/Webistes - Html/Chat App/src/firebase";
+
+const Login = () => {
+
+    const [err, setErr] = useState(false)
+  const navigate = useNavigate()
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+
+try {
+    await signInWithEmailAndPassword(auth, email, password);
+    navigate("/")
+}catch(err){
+  console.log(err);
+  setErr(true)
+}
+  }
+
+    return (
+        <div className="formContainer">
+            <div className="formWrapper">
+                <span className="logo">ChatNow</span>
+                <span className="title">Login</span>
+                <form onSubmit={handleSubmit}>
+                    <input type="email" placeholder='Your Email'/>
+                    <input type="password" placeholder='Password'/>
+                    <button>Log In</button>
+                    {err && <span>Something went wrong</span>}
+                </form>
+                <p>No account? <Link to="/register">Register</Link></p>
+            </div>
+        </div>
+      )
+}
+
+export default Login
