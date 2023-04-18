66101222eeb969e6e7b7667449b53f708597544d
Makssell <nimbusbamboo@gmail.com>
2023-04-18 22:37:11 +0200

    app.js update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
-import '@styles/globals.css'
+import Register from "../../Projects/Webistes - Html/Chat App/src/pages/Register";
+import Login from "../../Projects/Webistes - Html/Chat App/src/pages/Login";
+import Home from "../../Projects/Webistes - Html/Chat App/src/pages/Home";
+import "./style.scss"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../Projects/Webistes - Html/Chat App/src/context/AuthContext"
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  const ProtectedRoute = ({children}) => {
+    if(!currentUser){
+      return <Navigate to="/login"/>
+    }
+
+    return children
+
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+          <Route path="login" element={<Login/>}/>
+          <Route path="register" element={<Register/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+   
+  );
 }
 
-export default Application
+export default App;
